98ecc4fa255925368f7b1b0c3dd89eb7bf64b744
Emiils-T <emils.timma@gmail.com>
2025-01-05 21:15:51 +0200

    commit

--- bookmark/js/script.js
+++ bookmark/js/script.js
@@ -1,9 +1,38 @@
-const btn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
-btn.addEventListener('click',navToggle)
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+btn.addEventListener("click", navToggle);
 
-function navToggle(){
-    btn.classList.toggle('open')
-    menu.classList.toggle('flex')
-    menu.classList.toggle('hidden')
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
+
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
+
+// Tabs menu event listener
+
+tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+
+function onTabClick(e) {
+  //Deactivate all tabs
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+      "border-softRed",
+      "border-b-4",
+      "md:border-b-0"
+    );
+  });
+
+  //hide all panels
+  panels.forEach((panel) => panel.classList.add("hidden"));
+
+  //activate a new tab and panel based on target
+
+  e.target.classList.add('border-softRed', 'border-b-4')
+  const classString = e.target.getAttribute('data-target')
+  document.getElementById('panels')
+  .getElementsByClassName(classString)[0]
+  .classList.remove('hidden')
 }
