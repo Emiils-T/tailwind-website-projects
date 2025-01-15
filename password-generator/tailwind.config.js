d40faa852550a9a1ebfc287f15505867be3e4411
Emiils-T <emils.timma@gmail.com>
2025-01-15 14:46:50 +0200

    commit

---
+++ password-generator/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        darkBody: "#1C1C1C",
+        darkContainer: "#1F2937",
+        greenText: "#4ADF86",
+        grayText: "#D5D4D8",
+        borderColor: "#2F3E53",
+        buttonGreen: "#10B981",
+        outputGray: "#273549",
+        outputTextGreen: "#55F991",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        karla: ["Karla"],
+      },
+    },
+  },
+  plugins: [],
+};
